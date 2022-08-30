5188dfe8f0ed96389409fedd3411f649873a6cbd
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-08-31 00:27:46 +0530

    Time: 55 ms (91.20%), Space: 14.3 MB (18.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -2,7 +2,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         d = collections.Counter(magazine)
         for ch in ransomNote:
-            if ch not in d or d[ch] == 0:
+            if d[ch] == 0:
                 return False
             d[ch]-=1
         return True
