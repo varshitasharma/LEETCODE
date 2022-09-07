5f9e82e2aec23460724f0d1e753d8d93f24b8044
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-08 00:20:57 +0530

    Time: 1491 ms (28.97%), Space: 15.1 MB (22.60%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,9 +4,9 @@ class Solution:
         min_jumps = [float('inf')]*l
         min_jumps[0] = 0
         for i in range(l):
-            if i<= last:
-                if last <nums[i]+i:
-                    for index in range(i+1, min(l, nums[i]+i+1)):
-                        min_jumps[index] = min(min_jumps[index], min_jumps[i]+1)
-                    last = nums[i]+i
+            if i<= last and last <nums[i]+i:
+                # if last <nums[i]+i:
+                for index in range(i+1, min(l, nums[i]+i+1)):
+                    min_jumps[index] = min(min_jumps[index], min_jumps[i]+1)
+                last = nums[i]+i
         return min_jumps[-1]
