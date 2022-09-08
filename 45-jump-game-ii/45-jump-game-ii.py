1d7b8b2c740fb1702ebb80b97e363de50ca66092
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-08 12:51:42 +0530

    Time: 215 ms (45.13%), Space: 14.3 MB (54.57%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,11 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
+        '''O(N) Greedy'''
         curEnd, l, farthest, jumps = 0, len(nums), 0, 0
+        # Current end is the last index you can go 
         for i in range(l-1):
-            farthest = max(farthest, i+nums[i])
-            if i == curEnd:
-                jumps+=1
+            farthest = max(farthest, i+nums[i]) # Update farthest you can reach from any index
+            if i == curEnd:                     # While iterating, once you reach curEnd, update CurEnd with the farthest                         
+                jumps+=1                        #that you could reach till now and increment jump bcz now you have to jump to next curEnd
                 curEnd = farthest
         return jumps
+    
+    
+        '''O(N^2) DP'''
+        # l, last = len(nums),0
+        # min_jumps = [float('inf')]*l
+        # min_jumps[0] = 0
+        # for i in range(l):
+        #     if i<= last and last <nums[i]+i:
+        #         # if last <nums[i]+i:
+        #         for index in range(i+1, min(l, nums[i]+i+1)):
+        #             min_jumps[index] = min(min_jumps[index], min_jumps[i]+1)
+        #         last = nums[i]+i
+        # return min_jumps[-1]
         
