5bab7071556d6a4aa6fa4451d9b4693e05d25bd2
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-10 14:21:03 +0530

    Time: 46 ms (49.88%), Space: 13.7 MB (33.93%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -5,14 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        q = deque((1,digit) for digit in [1,2,3,4,5,6,7,8,9])
+        q = deque((1,digit) for digit in [1,2,3,4,5,6,7,8,9]) # Append all possible 1 digit numbers to queue as                                                                   tuple(length of num, num)
         output = []
         while(q):
-            length, num = q.popleft()
+            length, num = q.popleft()  # if length of num == required length then append to result
             if length == n: output.append(num)
             else:
-                for digit in range(10):
-                    if abs(num%10 - digit) == k: q.append((length+1, num*10 + digit))
+                for digit in range(10): # check for each possible next digit if difference between last digit of num
+                                        # and next digit is == k
+                    if abs(num%10 - digit) == k: q.append((length+1, num*10 + digit)) # if yes then add that digit to last of                                                                                       #current number and append it to q 
         return output
                         
                 
