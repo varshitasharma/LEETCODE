8359c54f5e6b86f45c349562306f05a4e5184c51
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-10 12:05:39 +0530

    Time: 53 ms (58.15%), Space: 17.3 MB (94.81%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.py
+++ 337-house-robber-iii/337-house-robber-iii.py
@@ -16,7 +16,19 @@ class Solution(object):
         left = self.dfs(root.left)
         right = self.dfs(root.right)
         return (root.val + left[1] + right[1], max(left[0],left[1]) + max(right[0],right[1]))
-            
+         
+    """
+    Input: [3,4,5,1,3,null,1]
+ input tree            dp tree:
+     3                  [3+3+1,4+5]
+    / \                /        \
+   4   5            [4,3]      [5,1]
+  / \   \          /     \          \
+ 1   2   1      [1,0]    [2,0]     [1,0]
+                / \       /  \        /  \
+           [0,0] [0,0] [0,0] [0,0]  [0,0] [0,0]
+    
+    """
         
     '''we construct a dp tree, each node in dp tree represents [rob the current node how much you gain, skip the     current node how much you gain]
     dp_node[0] =[rob the current node how much you gain]
