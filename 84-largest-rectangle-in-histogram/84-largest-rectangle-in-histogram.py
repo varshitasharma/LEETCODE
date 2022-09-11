8911f181806bbfd4345d14e1d0a18c5b80806612
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-11 11:13:49 +0530

    Time: 1870 ms (18.04%), Space: 26.1 MB (48.86%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -4,15 +4,16 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        heights.append(0)
-        stack = [-1]
+        heights.append(0) #Add dummy height so that when you reach last actual height, you have next index to compare it with
+        stack = [-1] 
+        # Add -1 so that you include left index as 0 for case when all elements to the left of current right were greater than that. So all elements would have got popped according to while condition. So left index must be equal to zero to get the width -> stack[top] must be equal to -1 -> to get left = 0 (-1 + 1)
         ans = 0
         for i in range(len(heights)):
-            while heights[i] < heights[stack[-1]]:
-                height = heights[stack.pop()]
-                width = i - stack[-1] - 1
+            while heights[i] < heights[stack[-1]]: # pop until prev height > currheight
+                height = heights[stack.pop()] 
+                width = i - stack[-1] - 1 # right = i-1, left = stack[-1]+1, width = right-left+1
                 ans = max(ans, height * width)
-            stack.append(i)
+            stack.append(i)  # push current index to stack
         heights.pop()
         return ans
             
