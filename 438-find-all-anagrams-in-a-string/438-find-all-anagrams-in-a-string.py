dfb43798ec2f7cfb27142429937915e660f09bd0
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-11 15:53:43 +0530

    Time: 253 ms (26.23%), Space: 14 MB (95.05%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -5,17 +5,18 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
-        letter_count_t = collections.Counter(t)
-        start, end, ans,  letter_count = 0,0, [], collections.Counter('')
+        letter_count_t = collections.Counter(t) #store count of letters of pattern
+        start, end, ans,  letter_count = 0,0, [], collections.Counter('')  # letter_count stores 0 for all letters by default
+                                                                        # it'll be used to store occurrences of all letters in a window
         t_len = len(t)
         while(end < len(s)):
-            if s[end] in t:
-                letter_count[s[end]]= letter_count[s[end]]+1
+            if s[end] in t:   # update letter count if the letter is in pattern
+                letter_count[s[end]]+=1
             if end-start+1 < t_len:
                 end+=1
-            elif end-start+1 == t_len:
-                if letter_count== letter_count_t : ans.append(start)
-                if s[start] in t: letter_count[s[start]]-=1
+            elif end-start+1 == t_len:  #when window length achieved 
+                if letter_count== letter_count_t : ans.append(start)  #if Counter of the actual matches with Counter of substring in current window
+                if s[start] in t: letter_count[s[start]]-=1   #before moving the window check if the start index(which will be out of window as window moves) contributes to letter count. If yes then decrease the letter count of that letter by 1
                 end+=1
                 start+=1
         return ans
