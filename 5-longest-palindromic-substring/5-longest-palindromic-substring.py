dc639513be630a1fc205f008eb1cc5278176b5e3
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-23 15:52:39 +0530

    Time: 1654 ms (37.84%), Space: 14 MB (57.86%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,4 +1,5 @@
 class Solution:
+    '''O(N^2) with O(1) space'''
     def longestPalindrome(self, s: str) -> str:
         res = ""
         for i in range(len(s)):
@@ -18,3 +19,28 @@ class Solution:
         while 0<=l <= r < len(s) and s[l] == s[r]:
             l -= 1; r += 1
         return s[l+1:r]
+    
+    
+    '''O(N^2) with O(N*N) space'''
+#     def longestPalindrome(self, s):
+#         longest_palindrom = ''
+#         dp = [[0]*len(s) for _ in range(len(s))]
+#         #filling out the diagonal by 1
+#         for i in range(len(s)):
+#             dp[i][i] = True
+#             longest_palindrom = s[i]
+			
+#         # filling the dp table
+#         for i in range(len(s)-1,-1,-1):
+# 				# j starts from the i location : to only work on the upper side of the diagonal 
+#             for j in range(i+1,len(s)):  
+#                 if s[i] == s[j]:  #if the chars mathces
+#                     # if len slicied sub_string is just one letter if the characters are equal, we can say they are palindomr dp[i][j] =True 
+#                     #if the slicied sub_string is longer than 1, then we should check if the inner string is also palindrom (check dp[i+1][j-1] is True)
+#                     if j-i ==1 or dp[i+1][j-1] is True:
+#                         dp[i][j] = True
+#                         # we also need to keep track of the maximum palindrom sequence 
+#                         if len(longest_palindrom) < len(s[i:j+1]):
+#                             longest_palindrom = s[i:j+1]
+                
+#         return longest_palindrom
