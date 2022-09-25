12111b63554f7a8e8302f09348b80fbde4444159
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-09-25 14:45:29 +0530

    Time: 32 ms (97.72%), Space: 13.9 MB (74.24%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -5,9 +5,9 @@ class Solution:
         """
         l, r = 0, len(matrix)-1
         while(l < r):
-            
+            top, bottom = l, r
             for i in range(r-l):
-                top, bottom = l, r
+                
                 topleft = matrix[top][l+i]
                 matrix[top][l+i] = matrix[bottom-i][l]
                 matrix[bottom-i][l] = matrix[bottom][r-i]
