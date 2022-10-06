d206527aa0e1ba9b6c4bd47c6feb4e74af84bb88
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-10-06 20:25:24 +0530

    Time: 1898 ms (100.00%), Space: 53.8 MB (26.25%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,20 +1,61 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        '''Binary search O(N + QlogN)'''
-        # A stores the indices of plates in 's'. 
-        A = [i for i,c in enumerate(s) if c == '|']
-        res = []
-        print(A)
+        '''O(N + Q)'''
+        '''
+        For each indice, find the nearest candle index on the left and on the right.
+
+Example:
+
+                        0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20
+
+                        *  *  *  |  *  *  |  *  *  *   *   *   |   *   *   |   |   *   *   |   *
+nearest right candle:   3  3  3  3  6  6  6  12 12 12  12 12  12  15  15  15   16  19  19  19  -
+nearest left candle:    -  -  -  3  3  3  6  6  6  6   6  6   12  12  12  15  16  16  16   19  19
+candle count:           0  0  0  1  1  1  2  2  2  2   2  2    3   3   3   4   5   5   5   6   6
+        '''
+        
+        n = len(s)
+        # print('len',n)
+        nearLeft, nearRight= [-1]*n,[-1]*n
+        candleCount, res = [0]*n, []
+        
+        candle = -1
+        for i in range(n-1,-1,-1):
+            if s[i] == '|': 
+                candle = i
+            nearRight[i] = candle
+        candle, count = -1,0
+        for i in range(0,n):
+            if s[i] == '|': 
+                candle = i
+                count+=1
+            nearLeft[i] = candle
+            candleCount[i] = count
+        # print(nearRight, nearLeft, candleCount)
         for a,b in queries:
-            i = bisect.bisect_left(A, a) # A[i] is the first index from 'a' to its right, where there's a plate
-            j = bisect.bisect(A, b) - 1  # A[j] is the first index from 'b' to its left, where there's a plate
-                          # bisect returns the index where b should be present in A, if b already exists in A, it returns next index to b in A, so we need to do -1 to get the right index
+            left, right = nearRight[a], nearLeft[b]
+            if left==-1 or right==-1 or left>=right : res.append(0)
+            else: res.append(right-left - (candleCount[right]-candleCount[left]) )
             
-            # Let E be, No. of elements between plates at j & i = A[j] - A[i] + 1
-            # Let P be, No. of plates between plates at j & i = j-i+1  (Since A stores indices of plates so if there's a plate between i & j, we'll  get it's count by j-i+1)
-            # No. of candles = E-P => A[j] - A[i] + 1 - (j-i+1) = A[j] - A[i]) - (j - i)
-            res.append((A[j] - A[i]) - (j - i) if i < j else 0)
         return res
+        
+        
+        
+        '''Binary search O(N + QlogN)'''
+#         # A stores the indices of plates in 's'. 
+#         A = [i for i,c in enumerate(s) if c == '|']
+#         res = []
+#         print(A)
+#         for a,b in queries:
+#             i = bisect.bisect_left(A, a) # A[i] is the first index from 'a' to its right, where there's a plate
+#             j = bisect.bisect(A, b) - 1  # A[j] is the first index from 'b' to its left, where there's a plate
+#                           # bisect returns the index where b should be present in A, if b already exists in A, it returns next index to b in A, so we need to do -1 to get the right index
+            
+#             # Let E be, No. of elements between plates at j & i including i,j= A[j] - A[i] + 1
+#             # Let P be, No. of plates between plates at j & i including plates at j&i= j-i+1  (Since A stores indices of plates so if there's a plate between i & j, we'll  get it's count by j-i+1)
+#             # No. of candles = E-P => A[j] - A[i] + 1 - (j-i+1) = A[j] - A[i]) - (j - i)
+#             res.append((A[j] - A[i]) - (j - i) if i < j else 0)
+#         return res
         
         
         
