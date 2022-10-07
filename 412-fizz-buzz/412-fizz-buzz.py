7a0e95959432a584f74994ca5e9cf5383909f37f
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-10-07 17:38:10 +0530

    Time: 45 ms (93.11%), Space: 15.1 MB (17.41%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,28 +1,29 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-                # ans list
-        ans = []
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+#                 # ans list
+#         ans = []
 
-        # Dictionary to store all fizzbuzz mappings
-        fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
+#         # Dictionary to store all fizzbuzz mappings
+#         fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
         
-        # List of divisors which we will iterate over.
-        divisors = [3, 5]
+#         # List of divisors which we will iterate over.
+#         divisors = [3, 5]
 
-        for num in range(1, n + 1):
+#         for num in range(1, n + 1):
 
-            num_ans_str = []
+#             num_ans_str = []
 
-            for key in divisors:
-                # If the num is divisible by key,
-                # then add the corresponding string mapping to current num_ans_str
-                if num % key == 0:
-                    num_ans_str.append(fizz_buzz_dict[key])
+#             for key in divisors:
+#                 # If the num is divisible by key,
+#                 # then add the corresponding string mapping to current num_ans_str
+#                 if num % key == 0:
+#                     num_ans_str.append(fizz_buzz_dict[key])
 
-            if not num_ans_str:
-                num_ans_str.append(str(num))
+#             if not num_ans_str:
+#                 num_ans_str.append(str(num))
 
-            # Append the current answer str to the ans list
-            ans.append(''.join(num_ans_str))
+#             # Append the current answer str to the ans list
+#             ans.append(''.join(num_ans_str))
 
-        return ans
+#         return ans
