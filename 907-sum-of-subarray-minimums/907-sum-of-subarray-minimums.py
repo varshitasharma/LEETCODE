36bd4f1231e73bff02ac0e0a0b51a73a1a5776de
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-10-08 23:44:49 +0530

    Time: 559 ms (81.76%), Space: 19.1 MB (21.03%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,4 +1,6 @@
 class Solution:
+    '''https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step'''
+    '''O(N)'''
     def sumSubarrayMins(self, arr: List[int]) -> int:
         #PLE- Previous Less Element, NLE - Next Less Element
         n, ans, mod = len(arr), 0, 1e9+7
@@ -10,18 +12,13 @@ class Solution:
             PLE[i] = stackP[-1] if stackP else  -1
             stackP.append(i)
         for i in range(n-1, -1, -1):
-            # print(stackN)
             while(stackN and arr[stackN[-1]] > arr[i] ):
                 stackN.pop()
             NLE[i] = stackN[-1] if stackN else n
             stackN.append(i)
-        # PLE[0], NLE[n-1] = -1, n
-        # print(NLE,PLE)
         for i in range(n):
-            # left = (i-PLE[i] if i!=PLE[i] else 1)
-            # right = (NLE[i]-i if i!=NLE[i] else 1)
-            ans += (arr[i]*((i-PLE[i])*(NLE[i]-i)))%mod
-            # print(ans)
+            ans += (arr[i]*((i-PLE[i])*(NLE[i]-i)))%mod  #i-PLE[i] gives no. of elements from i to it's left till where arr[i] is minimum. Similarly for i -PLE[i] on the right side.
+            # Product of both left*right gives the number of subarrays for which arr[i] is minimum.
         return int(ans%mod)
             
             
