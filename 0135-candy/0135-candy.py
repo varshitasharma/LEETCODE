82a65bc0a7faad325c341e361af50ad692cb9e0e
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-10-14 19:24:50 +0530

    Time: 235 ms (73.16%), Space: 16.8 MB (39.72%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,15 +1,25 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        candies = n
-        candy = [1]*n
-        sortedRating = sorted([ (i,rate) for i,rate in enumerate(ratings)], key = lambda x: x[1] )
-        for i,rate in sortedRating:
-            if i>0 and rate>ratings[i-1] and candy[i] <= candy[i-1]:
-                candies += candy[i-1]+1-candy[i]
-                candy[i] = candy[i-1]+1
-            if i<n-1 and rate>ratings[i+1] and candy[i] <= candy[i+1]:
-                candies += candy[i+1]+1-candy[i]
-                candy[i] = candy[i+1]+1
-        return candies
+        n =len(ratings)
+        candies = [1]*n
+        for i in range(1,n):
+            if ratings[i] > ratings[i-1]: candies[i] = candies[i-1]+1
+        for i in range(n-2,-1,-1):
+            if ratings[i] > ratings[i+1] and candies[i]<=candies[i+1]: candies[i] = candies[i+1]+1
+        return sum(candies)
+        
+        
+        '''O(NlogN)'''
+        # n = len(ratings)
+        # candies = n
+        # candy = [1]*n
+        # sortedRating = sorted([ (i,rate) for i,rate in enumerate(ratings)], key = lambda x: x[1] )
+        # for i,rate in sortedRating:
+        #     if i>0 and rate>ratings[i-1] and candy[i] <= candy[i-1]:
+        #         candies += candy[i-1]+1-candy[i]
+        #         candy[i] = candy[i-1]+1
+        #     if i<n-1 and rate>ratings[i+1] and candy[i] <= candy[i+1]:
+        #         candies += candy[i+1]+1-candy[i]
+        #         candy[i] = candy[i+1]+1
+        # return candies
     
