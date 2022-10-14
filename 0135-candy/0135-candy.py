4dcdc764e10a2bd16fecb17c75698bae67a9ce8c
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-10-14 17:49:59 +0530

    Time: 539 ms (6.90%), Space: 18.4 MB (7.69%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -4,18 +4,12 @@ class Solution:
         candies = n
         candy = [1]*n
         sortedRating = sorted([ (i,rate) for i,rate in enumerate(ratings)], key = lambda x: x[1] )
-        # print(sortedRating)
         for i,rate in sortedRating:
             if i>0 and rate>ratings[i-1] and candy[i] <= candy[i-1]:
-                # print(i, candy)
                 candies += candy[i-1]+1-candy[i]
                 candy[i] = candy[i-1]+1
             if i<n-1 and rate>ratings[i+1] and candy[i] <= candy[i+1]:
-                # print(i,candy)
                 candies += candy[i+1]+1-candy[i]
                 candy[i] = candy[i+1]+1
-                
-        
-        # print(candy)
         return candies
     
