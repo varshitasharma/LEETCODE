bdd270c055c822774032efbffa98c643ebc43b71
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-10-17 17:57:16 +0530

    Time: 1160 ms (75.10%), Space: 45.7 MB (41.51%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,13 +1,30 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        output, adj = [[] for _ in range(n)], defaultdict(list)
-        for source, dest in edges: adj[source].append(dest)
-        def dfs(parent, child): 
-            for dest in adj[child]:
-                if output[dest] and output[dest][-1] == parent: continue
-                output[dest].append(parent)
-                dfs(parent, dest)
-        for node in range(n):
-            dfs(node, node)
-        return output
-        
+        output, adj, indegree = [set() for _ in range(n)], defaultdict(list), [0]*n
+        for source, dest in edges: 
+            adj[source].append(dest)
+            output[dest].add(source)
+            indegree[dest]+=1
+            
+        q = deque( [u for u,degree in enumerate(indegree) if degree==0])
+        while(q):
+            parent = q.popleft()
+            for kid in adj[parent]:
+                output[kid].update(output[parent])
+                indegree[kid]-=1
+                if indegree[kid] == 0: q.append(kid)
+        return [sorted(a) for a in output]
+                
+                
+                
+        '''O(N^2)'''
+        # output, adj = [[] for _ in range(n)], defaultdict(list)
+        # for source, dest in edges: adj[source].append(dest)
+        # def dfs(parent, child): 
+        #     for dest in adj[child]:
+        #         if output[dest] and output[dest][-1] == parent: continue
+        #         output[dest].append(parent)
+        #         dfs(parent, dest)
+        # for node in range(n):
+        #     dfs(node, node)
+        # return output
