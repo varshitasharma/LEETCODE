301a1cdfc85d9571517adbb685829beb132f6ef9
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-11-02 15:18:31 +0530

    Time: 117 ms (51.10%), Space: 14.4 MB (56.38%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,44 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        # Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+        # The initial "rank" of each vertex is 1, because each of them is
+        # a standalone vertex with no connection to other vertices.
+        self.rank = [1] * size
+
+    # The find function here is the same as that in the disjoint set with path compression.
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = max([max(x) for x in edges]) 
+        # print(n)
+        uf = UnionFind(n)
+        # print(uf.root)
+        for e1, e2 in edges:
+            if not uf.connected(e1-1,e2-1): uf.union(e1-1,e2-1)
+            
+            else: return [e1,e2]
+            # print(e1,e2)
+            
+            
