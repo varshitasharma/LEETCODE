51e8caf3a567aac95b1f1ced88965ce430d59f75
varshitasharma <47267979+varshitasharma@users.noreply.github.com>
2022-11-03 19:26:04 +0530

    Time: 100 ms (35.12%), Space: 14 MB (74.70%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def union(self, x, y):
+        self.root[self.find(y)] = self.find(self.find(x))
+    
+    def find(self, x):
+        while(self.root[x] != x):
+            x = self.root[x]
+        return x
+    
+    def equationsPossible(self, equations: List[str]) -> bool:
+        self.root = { ch : ch for ch in [chr(i) for i in range(ord('a'),ord('z')+1)]}
+        for e in equations:
+            if e[1] == "=" :  self.union(e[0], e[3])
+        # print(self.root)
+        for e in equations:
+            # print(self.union(e[0], e[3]))
+            if e[1]=='!' and self.find(e[0]) == self.find(e[3]): return False
+        return True
+                
